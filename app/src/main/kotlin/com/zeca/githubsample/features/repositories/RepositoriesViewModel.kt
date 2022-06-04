1a028926db0d68372f8e29ecf0f49176224e18e5
Jose Carlos <jcaacar@gmail.com>
2022-06-04 19:06:03 -0300

    feature(repositories): remove stateIn from RepositoriesViewModel.repositories property

--- app/src/main/kotlin/com/zeca/githubsample/features/repositories/RepositoriesViewModel.kt
+++ app/src/main/kotlin/com/zeca/githubsample/features/repositories/RepositoriesViewModel.kt
@@ -8,16 +8,12 @@ import androidx.paging.cachedIn
 import androidx.paging.map
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import com.zeca.githubsample.data.repositories.RepoRepository
 import com.zeca.githubsample.features.repositories.ui.model.RepositoryUiModel
 import com.zeca.githubsample.features.repositories.ui.model.RepositoryUiModelMapper
-
-private const val TIMEOUT_FLOW_UNSUBSCRIBE = 5000L
 
 @HiltViewModel
 class RepositoriesViewModel @Inject constructor(
@@ -31,9 +27,5 @@ class RepositoriesViewModel @Inject constructor(
                 mapper.map(repo)
             }
         })
-    }.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(TIMEOUT_FLOW_UNSUBSCRIBE),
-        PagingData.empty()
-    ).cachedIn(viewModelScope)
+    }.cachedIn(viewModelScope)
 }
